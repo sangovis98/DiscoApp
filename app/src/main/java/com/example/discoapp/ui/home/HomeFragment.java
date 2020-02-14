f53264cbf96816fd1a051e4fc21c2958ee46a34d
sangovis98 <56151735+sangovis98@users.noreply.github.com>
2020-02-14 13:24:16 +0100

    Versión final 2.0

--- app/src/main/java/com/example/discoapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/discoapp/ui/home/HomeFragment.java
@@ -60,10 +60,10 @@ public class HomeFragment extends Fragment {
         mDatabase = FirebaseDatabase.getInstance().getReference();
         DatabaseReference uidRef = mDatabase.child("discotecas");
 
-        /** Codigo para insertar una discoteca, cambiad datos del constructor e incrementa el id por 1
-         Discoteca d = new Discoteca(3, "MoonDance", "imgRuta",rate, latitud, longitud);
-         mDatabase.child("discotecas").child(disco.getId()).setValue(disco);
-         */
+        //Codigo para insertar una discoteca, cambiad datos del constructor e incrementa el id por 1
+         Discoteca d = new Discoteca(6, "Joy Eslava", "Regueton/Techno", "@drawable/joy",3.5, 40.4171637, -3.7065661);
+         mDatabase.child("discotecas").child(d.getId()+"").setValue(d);
+
         uidRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
